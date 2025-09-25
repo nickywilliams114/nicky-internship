b31fcbe634d664b94f395152c05945e8c1e03128
Nicky <nickywilliams114@yahoo.com>
2025-09-25 09:51:23 -0600

    successfully hit API and placed 6 cards

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from 'axios';
 
 const HotCollections = () => {
+  const [ getCollection, setCollection ] = useState([]);
+
+  const fetchCollections = async () => {
+    const resp = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`);
+      console.log(resp.data);
+      setCollection(resp.data);  
+  };
+
+  useState(() => {
+    fetchCollections();
+  }, []);
+  
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,7 +28,7 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {new Array(6).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
