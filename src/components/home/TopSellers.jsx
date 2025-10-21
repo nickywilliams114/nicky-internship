5e3f57c7cb057cab7d4aff4008cd566a3d067168
Nicky <nickywilliams114@yahoo.com>
2025-10-21 10:50:58 -0600

    completed API request and dynamic data retrieval

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [ topSellers, setTopSellers ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+
+  const fetchTopSeller = async () => {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+    );
+    setTopSellers(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTopSeller();
+  }, []);
+
   return (
-    <section id="section-popular" className="pb-5">
+    <section id="section-top-sellers" className="pb-5">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -14,26 +30,55 @@ const TopSellers = () => {
             </div>
           </div>
           <div className="col-md-12">
-            <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
-            </ol>
+            {topSellers.length ? (
+              <ol className="author_list">
+                {topSellers.map((item) => (
+                  <li key={item.id}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${item.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={item.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${item.authorId}`}>
+                         {item.authorName}
+                      </Link>
+                      <span>{item.price} ETH</span>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            ) : (
+              <ol className="author_list">
+                {new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={``}>
+                        <Skeleton
+                          width="50px"
+                          height="50px"
+                          borderRadius="50%"
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={``}>
+                        <Skeleton width="100px" height="20px" />
+                      </Link>
+                      <span>
+                        <Skeleton width="40px" height="20px" />
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            )}
           </div>
         </div>
       </div>
